893bfb9e33e1fae5de5b297db22bb993b5cd4d47
csxiaocy <39528376+csxiaocy@users.noreply.github.com>
2018-11-09 21:22:14 +0800

    改为使用koa框架

--- index.js
+++ index.js
@@ -1,68 +1,33 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-// const fs = require('fs');
-// const mystu = require('./mystu');
-// const credit = require('./credit');
+const Koa = require('koa');
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
 const getCourses = require('./course');
-const morgan = require('morgan');
+const getClassmates = require('./classmate');
 
-const urlencodedParser = bodyParser.urlencoded({ extended: true });
-// HTTP请求体解析中间件
-app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser());
+app.use(async ctx => {
 
-// 打印日志中间件
-app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens['response-time'](req, res) + 'ms'
-    ].join('   ');
-}));
+    if (ctx.url === '/credit' && ctx.method === 'POST') {
+        let data = ctx.request.body;
+        let username = data.username;
+        let password = data.password;
+        let years = data.years;
+        let semester = data.semester;
+        getCourses(username, password, years, semester).then(value => {
+            let result = { 'classes': value };
+            ctx.body = result;
+        }).catch(value => {
+            ctx.body = value;
+        });
+    }
 
-// let username;
-// let password;
+    if (ctx.url === '/member' && ctx.method === 'POST') {
+        let class_id = ctx.request.body.class_id;
+        getClassmates(class_id).then(result => {
+            ctx.body = result;
+        });
+    }
 
-// app.get('/', function (req, res) {
-//     fs.readFile('index.html', 'utf-8', function (err, data) {
-//         if (err) {
-//             throw err;
-//         }
-//         res.end(data);
-//     });
-// });
-
-// app.post('/login', urlencodedParser, function (req, res) {
-
-//     let username = req.body.username;
-//     let password = req.body.password;
-
-//     // mystu(username, password).then(value => {
-//     //     console.log(value);
-//     //     res.send(value);
-//     //     res.end();
-//     // });
-
-//     credit(username, password).then(value => {
-//         console.log(value);
-//         res.send(value);
-//         res.end();
-//     });
-// });
-
-app.post('/course', urlencodedParser, function (req, res) {
-    let username = req.body.username;
-    let password = req.body.password;
-    let yearValue = req.body.yearValue;
-    let semesterIndex = req.body.semesterIndex;
-    getCourses(username, password, yearValue, semesterIndex).then(value => {
-        res.send(value);
-        res.end();
-    }).catch(value => {
-        res.send(value);
-        res.end();
-    });
 });
 
 app.listen(3000);
